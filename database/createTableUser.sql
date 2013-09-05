acc77aad37c5abdc523af964c65d205dcf2f2889
wrausch <waldemar.rausch@googlemail.com>
2013-09-05 23:48:08 +0200

    Add database sql files

---
+++ database/createTableUser.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nickName` varchar(155) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(155) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `isAdmin` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
