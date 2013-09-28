fc3279c23cbaab1ec5da0ef54f826b151aa1417f
wrausch <waldemar.rausch@googlemail.com>
2013-09-29 01:26:50 +0200

    Adjusted paging in backend for listing exams.

--- backend/View/listExam.php
+++ backend/View/listExam.php
@@ -1,67 +1,74 @@
 <html>
-	<head>
-		<link href="http://<?php echo $_SERVER['SERVER_NAME'];?>/style.css" type="text/css" rel="stylesheet">
-		<link rel="stylesheet" type="text/css" href="http://<?php echo $_SERVER['SERVER_NAME'];?>/backend/paging/css/style.css" media="screen"/>
-	</head>
-	<div class="header"> 
-		<img style="width: 88px;" alt="" src="../../exam.jpg">
+<head>
+    <link href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/style.css" type="text/css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css"
+          href="http://<?php echo $_SERVER['SERVER_NAME']; ?>/backend/View/paging/css/style.css" media="screen"/>
+</head>
+<div class="header">
+    <img style="width: 88px;" alt="" src="../../exam.jpg">
 		<span style="position: absolute; top: 10px; left: 111px; font-size: 31px; font-weight: 700;">
 			Exam preparation Manager
-		</span> 
-		<div class="logedUser shadow">
-			Sie sind angemeldet als: <?php echo $_SESSION['user'];?>
-		</div> 
-	</div>
-	<div class="menu">
-		<form method="post" name="logout" action="logout.php">
-			<input title="logout" class="cmsIcon" src="../../logout.png" name="logout" style="width:32px;float:left;margin-left:1px" type="image">
-		</form>
-		<input onClick="document.listExamQuestion.submit()" title="l&ouml;schen" class="cmsIcon" src="../../icons/delete_32.png" name="delete" type="image">
-		<form method="post" name="add" action="http://<?php echo $_SERVER['SERVER_NAME']; ?>/index2.php?action=add&amp;page=1">
-			<input title="hinzuf&uuml;gen" class="cmsIcon" src="../../icons/plus_32.png" name="add" type="image">
-		</form>
-	</div>
-	<div class="content">
-		<form name="listExamQuestion" method="post" action="/index2.php?action=delete&page=1">
-			<table style="width:100%;">
-			<tr>
-			    <th>#ID</th>
-			    <th>Frage</th>
-			    <th>Freischaltdatum</th>
-			    <th>Autor</th>
-			   <th>&nbsp;</th> 
-			   <th>&nbsp;</th> 
-		  </tr>  
-			<?php 
-				foreach($data as $row) {
-					?>
-					<tr>
-						<td> 
-							<?php echo $row['id']; ?>
-						</td>
-						<td>
-							<?php echo nl2br($row['question']) ?>
-						</td>
-						<td>
-							<?php echo date('d.m.Y', strtotime($row['startDate'])); ?>
-						</td>
-						<td>
-							<?php echo $row['author']; ?>
-						</td>
-						<td>
-							<input type="checkbox" name="selection[]" value="<?php echo $row['id']; ?>" />
-						</td>
-						<td>
-							<button onclick="location.href = '/index2.php?action=edit&id=<?php echo $row['id']; ?>';" class="cmsIcon" style="border: 0; background: transparent" type="button" title="bearbeiten">
-								<img alt="edit" src="../../icons/pencil_32.png">
-							</button>
-						</td>
-				</tr><?php
-				}
-			?>
-			</table>
-		</form>
-	</div>
+		</span>
+
+    <div class="logedUser shadow">
+        Sie sind angemeldet als: <?php echo $_SESSION['user']; ?>
+    </div>
+</div>
+<div class="menu">
+    <form method="post" name="logout" action="logout.php">
+        <input title="logout" class="cmsIcon" src="../../logout.png" name="logout"
+               style="width:32px;float:left;margin-left:1px" type="image">
+    </form>
+    <input onClick="document.listExamQuestion.submit()" title="l&ouml;schen" class="cmsIcon"
+           src="../../icons/delete_32.png" name="delete" type="image">
+
+    <form method="post" name="add"
+          action="http://<?php echo $_SERVER['SERVER_NAME']; ?>/index2.php?action=add&amp;page=1">
+        <input title="hinzuf&uuml;gen" class="cmsIcon" src="../../icons/plus_32.png" name="add" type="image">
+    </form>
+</div>
+<div class="content">
+    <form name="listExamQuestion" method="post" action="/index2.php?action=delete&page=1">
+        <table style="width:100%;">
+            <tr>
+                <th>#ID</th>
+                <th>Frage</th>
+                <th>Freischaltdatum</th>
+                <th>Autor</th>
+                <th>&nbsp;</th>
+                <th>&nbsp;</th>
+            </tr>
+            <?php
+            foreach ($data as $row) {
+                ?>
+                <tr>
+                <td>
+                    <?php echo $row['id']; ?>
+                </td>
+                <td>
+                    <?php echo nl2br($row['question']) ?>
+                </td>
+                <td>
+                    <?php echo date('d.m.Y', strtotime($row['startDate'])); ?>
+                </td>
+                <td>
+                    <?php echo $row['author']; ?>
+                </td>
+                <td>
+                    <input type="checkbox" name="selection[]" value="<?php echo $row['id']; ?>"/>
+                </td>
+                <td>
+                    <button onclick="location.href = '/index2.php?action=edit&id=<?php echo $row['id']; ?>';"
+                            class="cmsIcon" style="border: 0; background: transparent" type="button" title="bearbeiten">
+                        <img alt="edit" src="../../icons/pencil_32.png">
+                    </button>
+                </td>
+                </tr><?php
+            }
+            ?>
+        </table>
+    </form>
+</div>
 </html>
 		
 	
--- backend/View/paging/paging.php
+++ backend/View/paging/paging.php
@@ -1,72 +1,78 @@
 <?php
-require_once '/'.$_SERVER['DOCUMENT_ROOT'] .'/db.php';
+require_once '/' . $_SERVER['DOCUMENT_ROOT'] . '/db.php';
 $counterResult = mysql_query("SELECT COUNT(*) AS quantity FROM question");
 $result = mysql_fetch_assoc($counterResult);
-$count = $result['quantity']/10;
+$count = $result['quantity'] / 10;
 ?>
-        <style>
-            
-            a img{
-                border:none;
-                outline:none;
-            }
-            .content{
-                padding:0px;
-                bottom:0px;
-            }
-            .about{
-                width:100%;
-                height:400px;
-                background:transparent url(about.png) repeat-x top left;
-                border-top:2px solid #ccc;
-                border-bottom:2px solid #000;
-            }
-            .about .text{
-                width:16%;
-                margin:5px 2% 10px 2%;
-                height:380px;
-                float:left;
-                color:#FCFEF3;
-                font-size: 16px;
-                text-align:justify;
-                letter-spacing:0px;
-            }
-            .about .text h1{
-                border-bottom: 1px dashed #ccc;
-                color:#fff;
-            }
-            
-			.pagedemo{
-				border: 1px solid #CCC;
-				width:310px;
-				margin:2px;
-                padding:50px 10px;
-                text-align:center;
-				background-color:white;	
-			}
-        </style>
-            <div class="paging">
-                <div id="paging">                   
-                </div>
-            </div>
-		<script type="text/javascript" src="http://<?php echo $_SERVER['SERVER_NAME'];?>/backend/paging/jquery-1.3.2.js"></script>
-		<script src="http://<?php echo $_SERVER['SERVER_NAME'];?>/backend/paging/jquery.paginate.js" type="text/javascript"></script>
-		<script type="text/javascript">
-		$(function() {
-			$("#paging").paginate({
-				count 		: <?php echo $count;?>,
-				start 		: 1,
-				display     : 8,
-				border					: true,
-				border_color			: '#fff',
-				text_color  			: '#fff',
-				background_color    	: 'black',	
-				border_hover_color		: '#ccc',
-				text_hover_color  		: '#000',
-				background_hover_color	: '#fff', 
-				images					: false,
-				mouse					: 'press'
-			});
-		});
-		</script>
+<style>
+
+    a img {
+        border: none;
+        outline: none;
+    }
+
+    .content {
+        padding: 0px;
+        bottom: 0px;
+    }
+
+    .about {
+        width: 100%;
+        height: 400px;
+        background: transparent url(about.png) repeat-x top left;
+        border-top: 2px solid #ccc;
+        border-bottom: 2px solid #000;
+    }
+
+    .about .text {
+        width: 16%;
+        margin: 5px 2% 10px 2%;
+        height: 380px;
+        float: left;
+        color: #FCFEF3;
+        font-size: 16px;
+        text-align: justify;
+        letter-spacing: 0px;
+    }
+
+    .about .text h1 {
+        border-bottom: 1px dashed #ccc;
+        color: #fff;
+    }
+
+    .pagedemo {
+        border: 1px solid #CCC;
+        width: 310px;
+        margin: 2px;
+        padding: 50px 10px;
+        text-align: center;
+        background-color: white;
+    }
+</style>
+<div class="paging">
+    <div id="paging">
+    </div>
+</div>
+<script type="text/javascript"
+        src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/backend/View/paging/jquery-1.3.2.js"></script>
+<script src="http://<?php echo $_SERVER['SERVER_NAME']; ?>/backend/View/paging/jquery.paginate.js"
+        type="text/javascript"></script>
+<script type="text/javascript">
+    $(function () {
+        $("#paging").paginate({
+            count: <?php echo $count;?>,
+            start: 1,
+            display: 8,
+            border: true,
+            border_color: '#fff',
+            text_color: '#fff',
+            background_color: 'black',
+            border_hover_color: '#ccc',
+            text_hover_color: '#000',
+            background_hover_color: '#fff',
+            images: false,
+            mouse: 'press'
+        });
+    });
+</script>
 
