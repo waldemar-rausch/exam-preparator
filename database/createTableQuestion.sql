99b7a3a0f43474bc5ac7b851e8659b099a70742a
waldemar-rausch <waldemar.rausch@googlemai.com>
2014-02-14 09:03:38 +0100

    Update createTableQuestion.sql

--- database/createTableQuestion.sql
+++ database/createTableQuestion.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS `question` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `topic` varchar(255) COLLATE utf8mb4_bin NOT NULL,
+  `topic` varchar(255) COLLATE utf8_bin NOT NULL,
   `question` text CHARACTER SET utf8 NOT NULL,
   `startDate` date NOT NULL,
-  `author` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `author` varchar(100) COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin COMMENT='exam' AUTO_INCREMENT=80 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='exam' AUTO_INCREMENT=1 ;
