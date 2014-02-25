9c46ed99d252266dcba2935782ac2c4b9c955d6c
waldemar-rausch <waldemar.rausch@googlemai.com>
2014-02-25 14:01:52 +0100

    Create Solution.php

---
+++ Frontend/View/Solution.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Short description
+ *
+ * Long description
+ * Proprietary software created by hygi.de is closed source code. No licence is offered.
+ *
+ * @category
+ * @package
+ * @subpackage
+ * @copyright Copyright (c) 2004-2012 Hygi.de GmbH & Co. KG, Telgte, Germany
+ * @author wrausch
+ * @created 25.02.14 13:16
+ */
+foreach ($answerRows as $answerRow) {
+    echo htmlentities('Für die Frage: ').nl2br($answerRow['question']). ' <br />';
+    echo 'lautet die richtige Antwort: '.$answerRow['answer']. ' <br />' ;
+    echo htmlentities('Erläuterung: ').$answerRow['explanation']. ' <br /><hr>';
+}
+
+echo '<b>Bespreche bitte die Fragen mit deinen Kollegen.</b>';
+?>
+<form action="/logout.php" method="post">
+	<input type="submit" name="logout" value="logout" />
+</form>
