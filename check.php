7d6899c3b71ff1f7fdfa55c329143d711ddc55d7
waldemar-rausch <waldemar.rausch@googlemai.com>
2014-02-25 14:00:10 +0100

    Update check.php

--- check.php
+++ check.php
@@ -4,7 +4,7 @@ session_start();
 require_once '/'.$_SERVER['DOCUMENT_ROOT'] .'/Frontend/Controller/Exampreparation.php';
 
 
-$controller = new Frontend_Controller_ExamPrepartation();
+$controller = new Frontend_Controller_ExamPreparation();
 try{
     $controller->index();
 } catch (Exception $e) {
